d443c252ea39dc8e8cdf636ac5f60f2958806697
국산자몽 <50566360+K-Grapefruit@users.noreply.github.com>
2022-06-06 23:32:34 +0900

    chat & all browser send message

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,33 @@
-alert("hi");
+//fontend에서 backend로 연결하는 방법
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+const frontsocket = new WebSocket(`ws://${window.location.host}`);
+
+frontsocket.addEventListener("open", () => {
+  //socket이 connection에 open했을때 발생
+
+  console.log("Connected to Server ❤");
+});
+
+//server에서 메세지가 올 경우 응답
+frontsocket.addEventListener("message", (message) => {
+  console.log("New Message", message.data, "from the server");
+});
+
+frontsocket.addEventListener("close", () => {
+  console.log("Disconneted");
+});
+
+// setTimeout(() => {
+//   //frontend에서 backend로 메세지 보내기
+//   frontsocket.send("hello from the browser!");
+// }, 10000);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  frontsocket.send(input.value);
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
+import http from "http";
 import express from "express";
-
+import WebSocket from "ws";
 const app = express();
 
 app.set(`view engine`, "pug");
@@ -10,4 +11,48 @@ app.get("/", (req, res) => res.render("home"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
 
-app.listen(3000, handleListen);
+//express와 ws는 서로 다른 protocol 이므로 function을 구현해서 합쳐야함
+
+//http 서버
+const server = http.createServer(app);
+
+// WebSocket서버
+//서버를 전달(pass) 이러면 http , websocket서버 둘 다 돌릴 수 있음
+//필수는 아님 하나만 써도됨
+//http서버 위에 websocket 서버를 올림
+const wss = new WebSocket.Server({ server });
+
+//각기 다른 브라우저에서 연결될 때 / fakeDatabase
+const sockets = [];
+
+//socket은 어떤 사람 / 나(서버)와 브라우저 사이의 연결
+//connection이 생기면 socket을 얻음
+//이 메서드는 socket으로 직접적인 연결을 제공해줌
+//매번 새로운 브라우저가 backend로 연결할 때 backend와 연결된 각 브라우저에 대해 작동
+wss.on("connection", (backsocket) => {
+  console.log("Connected to Browser❤");
+
+  //각자 해당하는 브라우저 연결시 array에 넣어줌
+  //각 브라우저 연결이 될때마다 저장이 됨 , 연결된 브라우저마다 뿌려줌
+  sockets.push(backsocket);
+
+  //브라우저 창을 닫을 시 발생하는 이벤트
+  backsocket.on("close", () => {
+    console.log("disconnected from browser");
+  });
+  console.log(sockets);
+  //frontedn로 메세지 보내기
+  //backsocket.send("hello");
+
+  backsocket.on("message", (message) => {
+    // console.log(message.toString("utf8"));
+    // backsocket.send(message.toString("utf8"));
+    // 모든 socket을 거쳐서 해당하는 브라우저가 보낸 메세지를 다시 보냄
+    sockets.forEach((asocket) => asocket.send(message.toString("utf8")));
+  });
+});
+
+//서버는 http , ws2개의 protocol을 이해할 수 있게 됨
+server.listen(3000, handleListen);
+
+//app.listen(3000, handleListen);
